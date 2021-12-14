9fa4e9444c16af786ca53df72e8c8b51accb816d
Daehyung <bigmart0918@gmail.com>
2021-12-13 23:14:39 -0800

    ✔ Backend done

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,28 @@
 ## DO NOT WRITE ANY CODE IN THIS FILE
 ## backend code only!
+from fastapi import FastAPI, UploadFile, File, status, Response
+
+app = FastAPI()
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def main():
+    return {"message": "Hello World"}
+
+@app.post("/upload", status_code = status.HTTP_201_CREATED)
+async def upload_file(response: Response, file: UploadFile = File(...)):
+    try:
+        # if file is not .png or .jpg, raise 422 status code
+        if not file.filename.endswith(".png") and not file.filename.endswith(".jpg"):
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+            return {"message": "File format not supported"}
+        file_extension = file.filename[-4:]
+        #Write File in image + file_extension format
+        with open("image"+file_extension, "wb") as f:
+            f.write(file.file.read())
+    
+    #Raise 204 if error occurs
+    except Exception as e:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return {"message": str(e)}
+
+    return {"message": "File uploaded successfully"}
